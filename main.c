1272b294ff0a6ffe4de67d9e7945b1b80da64950
iivqum <59771955+iivqum@users.noreply.github.com>
2024-08-18 13:27:27 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 #include <SDL.h>
 
 // Number of program instructions an agent can have
-#define evo_agent_program_size 64
+#define evo_agent_program_size 128
 // Height and width of grid
 #define evo_grid_dimensions 256
-#define evo_state_population_size 1000
+#define evo_state_population_size 10000
 
-const int evo_state_max_iterations = 5000;
+const int evo_state_max_iterations = 300;
+
+const float evo_agent_mutation_chance = 0.1;
+const float evo_agent_selection_bias = 0.2;
 
 const int window_width = 512;
 const int window_height = 512;
@@ -21,8 +24,16 @@ typedef enum {
 	in_go_right,
 	in_go_up,
 	in_go_down,
+	in_top_left,
+	in_top_right,
+	in_bottom_left,
+	in_bottom_right,
 	in_do_nothing,
 	in_go_to_start,
+	in_next,
+	in_previous,
+	in_stop,
+	in_goto,
 	// End marker
 	in_last
 } instruction;
@@ -47,7 +58,9 @@ typedef struct {
 	//evo_cell grid[evo_grid_dimensions][evo_grid_dimensions];
 	evo_agent* current_generation;
 	evo_agent* next_generation;
+	int* best_agents;
 	float* fitnesses;
+	// Total fitness of the best agents
 	float total_fitness;
 	int iterations;
 	int generation;
@@ -66,8 +79,11 @@ void evo_agent_initialize(evo_agent* agent, evo_state* state, int x, int y) {
 }
 
 void evo_agent_move_to(evo_agent* agent, int new_x, int new_y) {
-	if (new_x < 0 || new_x >= evo_grid_dimensions || new_y < 0 || new_y >= evo_grid_dimensions) {
-		return;
+	if (new_x > 0 && new_x < evo_grid_dimensions) {
+		agent->x = new_x;
+	}
+	if (new_y > 0 && new_y < evo_grid_dimensions) {
+		agent->y = new_y;
 	}
 	/*
 	evo_cell* old_cell = &((evo_state*)agent->state)->grid[agent->x][agent->y];
@@ -80,12 +96,22 @@ void evo_agent_move_to(evo_agent* agent, int new_x, int new_y) {
 	new_cell->agent = agent;
 	new_cell->updated = true;
 	*/
-	agent->x = new_x;
-	agent->y = new_y;
+}
+
+instruction evo_agent_read_instruction(evo_agent* agent) {
+	instruction inst = agent->program[agent->step];
+
+	agent->step++;
+
+	if (agent->step >= evo_agent_program_size) {
+		agent->step = 0;
+	}
+
+	return inst;
 }
 
 void evo_agent_execute(evo_agent* agent) {
-	instruction inst = agent->program[agent->step];
+	instruction inst = evo_agent_read_instruction(agent);
 
 	switch (inst) {
 		case in_go_left:
@@ -100,14 +126,32 @@ void evo_agent_execute(evo_agent* agent) {
 		case in_go_down:
 			evo_agent_move_to(agent, agent->x, agent->y + 1);
 			break;
-		case in_go_to_start:
-			//agent->step = 0;
+		case in_top_left:
+			evo_agent_move_to(agent, agent->x - 1, agent->y - 1);
 			break;
-	}
-	agent->step++;
-
-	if (agent->step >= evo_agent_program_size) {
-		agent->step = 0;
+		case in_top_right:
+			evo_agent_move_to(agent, agent->x + 1, agent->y - 1);
+			break;
+		case in_bottom_left:
+			evo_agent_move_to(agent, agent->x - 1, agent->y + 1);
+			break;
+		case in_bottom_right:
+			evo_agent_move_to(agent, agent->x + 1, agent->y + 1);
+			break;
+		case in_go_to_start:
+			agent->step = 0;
+			break;
+		case in_next:
+			agent->step++;
+			break;
+		case in_previous:
+			agent->step--;
+			break;
+		case in_stop:
+			agent->dead = true;
+			break;
+		case in_goto:
+			break; 
 	}
 }
 
@@ -137,7 +181,7 @@ void evo_state_setup(evo_state* state) {
 			break;
 		}
 		*/
-		evo_agent_initialize(agent, state, 0, evo_grid_dimensions * 0.5);
+		evo_agent_initialize(agent, state, 0, 0);
 		//y++;
 	}
 }
@@ -146,12 +190,14 @@ void evo_state_cleanup(evo_state* state) {
 	free(state->current_generation);
 	free(state->next_generation);
 	free(state->fitnesses);
+	free(state->best_agents);
 }
 
 void evo_state_initialize(evo_state* state) {
 	state->current_generation = malloc(sizeof(evo_agent) * evo_state_population_size);
 	state->next_generation = malloc(sizeof(evo_agent) * evo_state_population_size);
 	state->fitnesses = malloc(sizeof(float) * evo_state_population_size);
+	state->best_agents = malloc(sizeof(int) * evo_state_population_size * evo_agent_selection_bias);
 
 	for (int i = 0; i < evo_state_population_size; i++) {
 		evo_agent_randomize_program(&state->current_generation[i]);
@@ -169,26 +215,61 @@ float evo_agent_get_fitness(evo_agent* agent) {
 	float dy = ((float)agent->y + 0.5) - center;
 	//float distance = sqrtf(dx * dx);
 	float fitness = (fabs(dx) + fabs(dy)) / (float)evo_grid_dimensions;
-	return 1 - fitness;
+
+	float dist = sqrt(dx * dx + dy * dy);
+
+	return 1 - (dist / 190);
 }
 
 evo_agent* evo_state_select_agent(evo_state* state) {
 	float random_choice = (float)(((double)rand() / (double)RAND_MAX)) * state->total_fitness;
+	int amount = evo_state_population_size * evo_agent_selection_bias;
 	float total = 0;
-	for (int i = 0; i < evo_state_population_size; i++) {
-		total += state->fitnesses[i];
+
+	for (int i = 0; i < amount; i++) {
+		int index = state->best_agents[i];
+		total += state->fitnesses[index];
 		if (total >= random_choice) {
-			return &state->current_generation[i];
+			return &state->current_generation[index];
 		}
 	}
+	printf("Invalid agent!\nTotal fitness = %f", state->total_fitness);
 	return NULL;
 }
 
+void shift_array_int(int* arr, int start, int len) {
+	int last_index = len - 1;
+	for (int i = last_index - 1; i >= start; i--) {
+		arr[last_index] = arr[i];
+		last_index = i;
+	}
+}
+
 void evo_state_compute_fitness(evo_state *state) {
-	float total_fitness = 0;
+	// Calculate fitness for each agent
 	for (int i = 0; i < evo_state_population_size; i++) {
 		state->fitnesses[i] = evo_agent_get_fitness(&state->current_generation[i]);
-		total_fitness += state->fitnesses[i];
+	}
+	// Select top agents 
+	int amount = evo_state_population_size * evo_agent_selection_bias;
+
+	memset(state->best_agents, -1, sizeof(int) * amount);
+
+	for (int i = 0; i < evo_state_population_size; i++) {
+		for (int j = 0; j < amount; j++) {
+			int index = state->best_agents[j];
+			if (index == -1 || state->fitnesses[i] > state->fitnesses[index]) {
+				shift_array_int(state->best_agents, j, amount);
+				state->best_agents[j] = i;
+				break;
+			}
+		}
+	}
+	// Calculate total fitness of the best agents
+	float total_fitness = 0;
+	for (int i = 0; i < amount; i++) {
+		int index = state->best_agents[i];
+		total_fitness += state->fitnesses[index];
 	}
 	state->total_fitness = total_fitness;
 }
@@ -196,8 +277,15 @@ void evo_state_compute_fitness(evo_state *state) {
 void evo_agent_breed(evo_agent* child, evo_agent* parent_1, evo_agent* parent_2) {
 	int split = rand() % evo_agent_program_size;
 
-	memcpy(child->program, parent_1->program, sizeof(instruction) * split);
-	memcpy(child->program + (evo_agent_program_size - split), parent_2->program, sizeof(instruction) * (evo_agent_program_size - split + 1));
+	memcpy(child->program, parent_1->program, sizeof(instruction) * (split + 1));
+	memcpy(child->program + split + 1, parent_2->program, sizeof(instruction) * (evo_agent_program_size - split + 1));
+
+	int mutation_chance = rand() % (int)(1 / evo_agent_mutation_chance);
+
+	if (mutation_chance == 0) {
+		int mutate_index = rand() % evo_agent_program_size;
+		child->program[mutate_index] = rand() % in_last;
+	}
 }
 
 void evo_state_evolve(evo_state* state) {
@@ -212,7 +300,13 @@ void evo_state_evolve(evo_state* state) {
 	}
 	memcpy(state->current_generation, state->next_generation, sizeof(evo_agent) * evo_state_population_size);
 	evo_state_setup(state);
-	printf("Total fitness = %f, generation = %d\r", state->total_fitness, state->generation);
+
+	float total_fitness = 0;
+	for (int i = 0; i < evo_state_population_size; i++) {
+		total_fitness += state->fitnesses[i];
+	}
+
+	printf("Total fitness = %f, generation = %d\r", total_fitness, state->generation);
 }
 
 void evo_state_update(evo_state* state) {
@@ -220,7 +314,7 @@ void evo_state_update(evo_state* state) {
 
 	for (int i = 0; i < evo_state_population_size; i++) {
 		agent = &state->current_generation[i];
-		if (agent->updated == true) {
+		if (agent->updated == true || agent->dead == true) {
 			continue;
 		}
 		evo_agent_execute(agent);
